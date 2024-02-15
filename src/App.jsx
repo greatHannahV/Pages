bb517c1c17878b3b1832b156b423b17e8eaa6af9
greatHannahV <i.kasl@mail.ru>
2024-02-15 21:26:16 +0200

    changed the batn parh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
-import Menu from '../pages/Menu'
-import AppLayout from '../pages/AppLayout'
-import About from '../pages/About'
-import Address from '../pages/Address'
-import Contacts from '../pages/Contacts'
-import Settings from '../pages/Settings'
-import NotFound from '../pages/NotFound'
+
+import AppLayout from './pages/AppLayout'
+import About from './pages/About'
+import Address from './pages/Address'
+import Contacts from './pages/Contacts'
+import Settings from './pages/Settings'
+
+import Menu from './pages/Menu'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router'
-import Button from '../Button'
+import Button from '../components/Button'
 
 function AppLayout() {
   return (
