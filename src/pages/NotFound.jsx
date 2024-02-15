f55d31f2fd5e1c0e3a9f04dbbde9d65221cc46ff
greatHannahV <i.kasl@mail.ru>
2024-02-15 21:20:30 +0200

    added btn to the err file

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,11 @@
+import Button from '../Button'
 function NotFound() {
-  return <div>Error</div>
+  return (
+    <div>
+      <p>Error</p>
+      <Button />
+    </div>
+  )
 }
 
 export default NotFound
